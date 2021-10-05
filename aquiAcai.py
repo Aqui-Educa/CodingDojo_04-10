7425dc93f801d8da793515d435d7e889515f1e30
Marlon Almino <alminomarlon@gmail.com>
2021-10-04 21:45:10 -0300

    implementando funcao verificarTamanho

--- aquiAcai.py
+++ aquiAcai.py
@@ -6,17 +6,31 @@ p = 9.25
 m = 18.50
 g = 27.75
 
-tamanho = str(input("Informe o tamanho desejado? "))
+tamanho = str(input("Informe o tamanho desejado? ")).lower()
 adicional = int(input("Deseja quantos adicionais? "))
 
 def verificarTamanho(tamanho):
-    while tamanho not in ["p", "m", "g"]:
-        print('Infome se P, M ou G: ')
-        tamanho = str(input("Informe o tamanho desejado? "))
-        return
+    while tamanho not in ['p', 'm', 'g']:
+        
+   else:
+       if tamanho == 'p':
+            tamanho = p
+        
+        if tamanho == 'm':
+            tamanho = m
+        
+        if tamanho == 'g':
+            tamanho = g
+            
+        return tamanho
+    #     print('Infome se P, M ou G: ')
+    #     tamanho = str(input("Informe o tamanho desejado? ")).lower()
+    # else:
+    #     pass
 
 def preco(tamanho, adicional):
     preco = tamanho + (adicional*2.50)
     return preco
 
-print(preco(p, adicional))
+print(verificarTamanho(tamanho))
+print(preco(tamanho, adicional))
